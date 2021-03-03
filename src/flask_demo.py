f888dbcc78bd6f2cbf3acbfff9ebee381fb3dfd3
Nikolay Korolev <korolevns98@gmail.com>
2021-03-03 14:07:10 +0300

    try this

--- src/flask_demo.py
+++ src/flask_demo.py
@@ -10,7 +10,6 @@ from src.data_processor.path_utils import get_model_dir
 from src.parse_args import args
 from src.trans_checker.args import args as cs_args
 from src.app.forms import SQLForm
-from src.semantic_parser.learn_framework import EncoderDecoderLFramework
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '{}'.format(args.gpu)
 app = Flask(__name__, template_folder='templates')
@@ -18,21 +17,7 @@ app.config['SECRET_KEY'] = os.urandom(32)
 
 
 def get_answer(db_name, query):
-    db_path = os.path.join(args.db_dir, db_name, '{}.sqlite'.format(db_name))
-    schema = SchemaGraph(db_name, db_path=db_path)
-
-    import json
-    in_json = os.path.join(args.data_dir, 'tables.json')
-    with open(in_json) as f:
-        tables = json.load(f)
-    for table in tables:
-        if table['db_id'] == db_name:
-            break
-    schema.load_data_from_spider_json(table)
-
-    t2sql = Text2SQLWrapper(args, cs_args, schema)
-
-    output = t2sql.process(query, schema.name)
+    output = t2sql.process(query, db_name)
     translatable = output['translatable']
     sql_query = output['sql_query']
     confusion_span = output['confuse_span']
@@ -59,9 +44,22 @@ if __name__ == '__main__':
     with torch.no_grad():
         args.model_id = utils.model_index[args.model]
         get_model_dir(args)
-        # if args.model in ['bridge',
-        #                   'seq2seq',
-        #                   'seq2seq.pg']:
-        #     sp = EncoderDecoderLFramework(args)
-        #     sp.cuda()
+
+        t2sql = Text2SQLWrapper(args, cs_args, None)
+        for db_name in ['academic']:
+            db_path = os.path.join(args.db_dir, db_name, '{}.sqlite'.format(db_name))
+            schema = SchemaGraph(db_name, db_path=db_path)
+
+            import json
+
+            in_json = os.path.join(args.data_dir, 'tables.json')
+            with open(in_json) as f:
+                tables = json.load(f)
+            for table in tables:
+                if table['db_id'] == db_name:
+                    break
+
+            schema.load_data_from_spider_json(table)
+            t2sql.add_schema(schema)
+
         app.run(host='0.0.0.0', port=8080)
