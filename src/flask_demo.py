e065dd42f0742990b2ac869c4a49b8db13738f59
Nikolay Korolev <korolevns98@gmail.com>
2021-03-03 13:58:03 +0300

    Try to do

--- src/flask_demo.py
+++ src/flask_demo.py
@@ -1,16 +1,46 @@
 import torch
+from asciitree import LeftAligned
 from flask import Flask, render_template, request
 import os
 
+from src.data_processor.schema_graph import SchemaGraph
+from src.demos.demos import Text2SQLWrapper
 from src.utils import utils
 from src.data_processor.path_utils import get_model_dir
 from src.parse_args import args
+from src.trans_checker.args import args as cs_args
 from src.app.forms import SQLForm
 from src.semantic_parser.learn_framework import EncoderDecoderLFramework
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '{}'.format(args.gpu)
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = os.urandom(32)
+
+
+def get_answer(db_name, query):
+    db_path = os.path.join(args.db_dir, db_name, '{}.sqlite'.format(db_name))
+    schema = SchemaGraph(db_name, db_path=db_path)
+
+    import json
+    in_json = os.path.join(args.data_dir, 'tables.json')
+    with open(in_json) as f:
+        tables = json.load(f)
+    for table in tables:
+        if table['db_id'] == db_name:
+            break
+    schema.load_data_from_spider_json(table)
+
+    t2sql = Text2SQLWrapper(args, cs_args, schema)
+
+    output = t2sql.process(query, schema.name)
+    translatable = output['translatable']
+    sql_query = output['sql_query']
+    confusion_span = output['confuse_span']
+    replacement_span = output['replace_span']
+    answer = [LeftAligned()(schema.printable), 'SQL: {}'.format(sql_query), 'Translatable: {}'.format(translatable),
+              'Confusion span: {}'.format(confusion_span), 'Replacement span: {}'.format(replacement_span)]
+    answer = "\n".join(answer)
+    return answer
 
 
 @app.route('/', methods=['GET', 'POST'])
